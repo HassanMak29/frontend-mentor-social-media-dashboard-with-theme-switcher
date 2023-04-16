a7b5e438ea05f095ba92895b3d84273586ec2b16
HassanMak29 <76273304+HassanMak29@users.noreply.github.com>
2023-04-16 10:20:44 +0100

    persisted theme in localstorage and refactored

--- main.js
+++ main.js
@@ -1,44 +1,42 @@
 const switcher = document.querySelector(".switcher");
 const switchBtn = document.querySelector(".switch");
+const root = document.querySelector(":root");
+
+const lightTheme = {
+  "--BG": "hsl(0, 0%, 100%)",
+  "--TopBGPattern": "hsl(225, 100%, 98%)",
+  "--CardBG": "hsl(227, 47%, 96%)",
+  "--SecondaryText": "hsl(228, 12%, 44%)",
+  "--Text": "hsl(230, 17%, 14%)",
+};
+const darkTheme = {
+  "--BG": "hsl(230, 17%, 14%)",
+  "--TopBGPattern": "hsl(232, 19%, 15%)",
+  "--CardBG": "hsl(228, 28%, 20%)",
+  "--SecondaryText": "hsl(228, 34%, 66%)",
+  "--Text": "hsl(0, 0%, 100%)",
+};
+
+if (localStorage.getItem("Social-media-dashboard-theme")) {
+  switchBtn.classList.add("light");
+  Object.entries(lightTheme).forEach((v) => root.style.setProperty(v[0], v[1]));
+}
 
 switcher.addEventListener("click", () => {
   switchBtn.classList.toggle("light");
 
   if (switchBtn.classList.contains("light")) {
-    document.documentElement.style.setProperty("--BG", "hsl(0, 0%, 100%)");
-    document.documentElement.style.setProperty(
-      "--TopBGPattern",
-      "hsl(225, 100%, 98%)"
-    );
-    document.documentElement.style.setProperty(
-      "--CardBG",
-      "hsl(227, 47%, 96%)"
-    );
-    document.documentElement.style.setProperty(
-      "--SecondaryText",
-      "hsl(228, 12%, 44%)"
-    );
-    document.documentElement.style.setProperty(
-      "--White-Text",
-      "hsl(230, 17%, 14%)"
+    localStorage.setItem("Social-media-dashboard-theme", "light");
+
+    Object.entries(lightTheme).forEach((v) =>
+      root.style.setProperty(v[0], v[1])
     );
   } else {
-    document.documentElement.style.setProperty("--BG", "hsl(230, 17%, 14%)");
-    document.documentElement.style.setProperty(
-      "--TopBGPattern",
-      "hsl(232, 19%, 15%)"
-    );
-    document.documentElement.style.setProperty(
-      "--CardBG",
-      "hsl(228, 28%, 20%)"
-    );
-    document.documentElement.style.setProperty(
-      "--SecondaryText",
-      "hsl(228, 34%, 66%)"
-    );
-    document.documentElement.style.setProperty(
-      "--White-Text",
-      "hsl(0, 0%, 100%)"
+    localStorage.getItem("Social-media-dashboard-theme") &&
+      localStorage.removeItem("Social-media-dashboard-theme");
+
+    Object.entries(darkTheme).forEach((v) =>
+      root.style.setProperty(v[0], v[1])
     );
   }
 });
